3dfa2dbc98f1cd25b8f072f3f5573319ac8329b1
Tu Nguyen <im2nguyen@users.noreply.github.com>
2021-02-11 14:38:05 -0800

    Merge outputs from two modules

--- main.tf
+++ main.tf
@@ -1,35 +1,18 @@
-module "random_pets" {
+module "random_pets_first" {
   source = "./modules/random_pet"
 
-  for_each        = toset(["1", "2", "3", "4"])
+  for_each        = toset(["1", "2"])
   pet_name_length = each.key
 }
 
-output "random_pet_names_module" {
+module "random_pets_second" {
+  source = "./modules/random_pet"
+
+  for_each        = toset(["3", "4"])
+  pet_name_length = each.key
+}
+
+output "random_pet_names" {
   description = "Random pet names"
-  value       = module.random_pets
-}
-
-output "random_pet_names_map" {
-  description = "Random pet names, convert to map"
-  value       = { for k in sort(keys(module.random_pets)) : k => module.random_pets[k].pet_name }
-}
-
-output "random_pet_names_map_values_only" {
-  description = "Random pet names values only"
-  value       = { for k in sort(keys(module.random_pets)) : k => module.random_pets[k].pet_name }
-}
-
-output "random_pet_names_list" {
-  description = "Random pet names convert to list"
-  value       = tolist(values({ for k in sort(keys(module.random_pets)) : k => module.random_pets[k].pet_name }))
-}
-
-locals {
-  bd = tolist(values({ for k in sort(keys(module.random_pets)) : k => module.random_pets[k].pet_name }))
-}
-
-output "bd" {
-  description = "Random pet names convert to list"
-  value       = local.bd
+  value       = merge(module.random_pets_first, module.random_pets_second)
 }
